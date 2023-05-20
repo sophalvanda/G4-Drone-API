8fb334bade13a4ab8f2fc6950cc441029ab5cf6c
sreyrea <hansreyrea2222@gmail.com>
2023-05-20 10:54:25 +0700

    [G4-04]create table farmers

---
+++ Code/app/Http/Controllers/FarmerController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Farmer;
+use Illuminate\Http\Request;
+
+class FarmerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Farmer $farmer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Farmer $farmer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Farmer $farmer)
+    {
+        //
+    }
+}
