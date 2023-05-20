77568a2106f466d39d610df585014dee1c97e672
sreyrea <hansreyrea2222@gmail.com>
2023-05-20 12:20:04 +0700

    [G4-07] seeder all table

--- Code/database/seeders/DatabaseSeeder.php
+++ Code/database/seeders/DatabaseSeeder.php
@@ -14,9 +14,52 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::create([
+            'name' => 'Test User',
+            'email' => 'user2@example.com',
+            'password' => 'user2@example.com',
+        ]);
+
+        \App\Models\Drone::create([
+            'name' => 'Drone 1',
+            'type' => 'Quadcopter',
+            'battery' => '3000mAh',
+            'payload-capacity' => '500g',
+            'user_id' => 1
+        ]);
+
+        \App\Models\Plan::create([
+            'name' => 'Plan 1',
+            'type' => 'Survey',
+            'datetime' => '2023-06-01 09:00:00',
+            'instruction' => 'Survey the farm fields',
+            'area' => '100 acres',
+            'user_id' => 1
+        ]);
+
+        \App\Models\DronePlane::create([
+            'drone_id' => 1,
+            'plan_id' => 1
+        ]);
+
+        \App\Models\Location::create([
+            'city' => 'PP',
+            'province' => 'BTB'
+        ]);
+        \App\Models\Farm::create([
+            'name' => 'Waypoint 1',
+            'latitude' => '49.2827',
+            'longitude' => '-123.1207',
+            'plan_id' => 1,
+            'user_id' => 1
+        ]);
+        \App\Models\Map::create([
+            'name' => 'Farm 1 Map',
+            'image_url' => 'https://example.com/farm1-map.jpg',
+            'farm_id' => 1,
+            'drone_id' => 1,
+            'location_id' => 1
+        ]);
+       
     }
 }
