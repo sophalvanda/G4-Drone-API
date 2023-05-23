702f18070e905a8c69c2863290792f4c52d3a898
sreyrea <hansreyrea2222@gmail.com>
2023-05-23 20:43:01 +0700

    [G4-13] get list of drone

--- Code/app/Http/Controllers/DroneController.php
+++ Code/app/Http/Controllers/DroneController.php
@@ -13,6 +13,8 @@ class DroneController extends Controller
     public function index()
     {
         //
+        $drone =Drone::all();
+        return response()->json(['message'=>'Request successfully!','data'=>$drone]);
     }
 
     /**
@@ -21,14 +23,33 @@ class DroneController extends Controller
     public function store(Request $request)
     {
         //
+        $drone = Drone::create([
+            'name' => request('name'),
+            'type' => request('type'),
+            'battery' => request('battery'),
+            'payload-capacity' => request('payload-capacity'),
+            'user_id' => request('user_id'),
+            'location_id' => request('location_id'),
+        ]);
+        
+        return response()->json(['message'=>'Create successfully!','data'=>$drone]);
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Drone $drone)
+    public function show( $id)
     {
-        //
+        $drone = Drone::find($id);
+        return Response()->json(['message'=>'Show successfully!','data'=>$drone],200);
+
+    }
+    public function showLocation($id)
+    {
+        $drone = Drone::with('location')->get();
+        return Response()->json(['message'=>'Show successfully!','data'=>$drone],200);
+
     }
 
     /**
--- Code/app/Models/Location.php
+++ Code/app/Models/Location.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Location extends Model
@@ -20,5 +21,10 @@ class Location extends Model
     public function Map(): HasOne
     {
         return $this->hasOne(Map::class);
+    }
+
+    public function drone(): HasMany
+    {
+        return $this->hasMany(Location::class);
     }
 }
